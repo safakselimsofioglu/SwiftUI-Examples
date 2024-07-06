a50cf8674a53b811e119ad6aabf76b7929677bf0
safakselimsofioglu <154444832+safakselimsofioglu@users.noreply.github.com>
2024-07-06 17:32:58 +0300

    Keyframe animation example

---
+++ SwiftUI 5.0 Tutorials - WWDC 2023/Keyframes App/Keyframes App/Keyframes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keyframes_AppApp.swift
+//  Keyframes App
+//
+//  Created by Şafak Selim Sofioğlu on 6.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Keyframes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
